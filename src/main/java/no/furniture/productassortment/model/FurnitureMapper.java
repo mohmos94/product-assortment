8140aba3b159468a500194cba657a48c263fd3be
mosti <mosti4@hotmail.com>
2024-02-18 21:39:17 +0100

    Laget foreløpig enkelt kontroller endepunkt for sjekk om data og jdbc template utspørring

---
+++ src/main/java/no/furniture/productassortment/model/FurnitureMapper.java
@@ -0,0 +1,8 @@
+package no.furniture.productassortment.model;
+
+public record FurnitureMapper (
+        String item,
+        String name,
+        double weight
+){
+}
--- src/main/java/no/furniture/productassortment/model/items/Furniture.java
+++ src/main/java/no/furniture/productassortment/model/items/Furniture.java
@@ -1,7 +1,6 @@
 package no.furniture.productassortment.model.items;
 
 public record Furniture (
-        int itemNumber,
         String item,
         String name,
         double weight){
