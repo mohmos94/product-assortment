db2392cfdf1c741456d83ba3d08c2407be3d0b71
mosti <mosti4@hotmail.com>
2024-02-18 17:59:03 +0100

    initial commit

---
+++ src/main/java/no/furniture/productassortment/ProductAssortmentApplication.java
@@ -0,0 +1,13 @@
+package no.furniture.productassortment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAssortmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAssortmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/furniture/productassortment/ProductAssortmentApplicationTests.java
@@ -0,0 +1,13 @@
+package no.furniture.productassortment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAssortmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
